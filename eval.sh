79cc0cc40ca1bae2f29a8ae5ee9362156c002428
bingli <bingli@deeproute.ai>
2025-11-30 08:41:06 +0800

    eval: update checkpoints file

--- eval.sh
+++ eval.sh
@@ -4,7 +4,7 @@ DATA_DIR="/home/libing/dataset/nuscene-mini/"
 # there should be ${DATA_DIR}/full_v1.0/
 # and also ${DATA_DIR}/mini
 
-MODEL_NAME="1x5_3e-4_rgb00_20:04:28"
+MODEL_NAME="8x5_5e-4_rgb12_22:43:46"
 
 EXP_NAME="00" # evaluate rgb00 model
 
@@ -15,5 +15,5 @@ python eval_nuscenes.py \
        --data_dir=$DATA_DIR \
        --log_dir='logs_eval_nuscenes' \
        --init_dir="checkpoints/${MODEL_NAME}" \
-       --res_scale=1 \
+       --res_scale=2 \
        --device_ids=[0]
